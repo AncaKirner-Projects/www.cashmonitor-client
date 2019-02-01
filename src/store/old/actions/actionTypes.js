a4d80a9207915f925cb15e6530a5ec5fcf1b1c52
Anca <ancat89@gmail.com>
2019-02-01 14:11:46 +0200

    remove old part

--- src/store/old/actions/actionTypes.js
+++
@@ -1,8 +0,0 @@
-const ActionTypes = {
-  LOGIN_USER: 'user:login',
-  LOGOUT_USER: 'user:logout',
-  ADD_INCOME: 'money:addIncome',
-  ADD_SPENT: 'money:addSpent',
-};
-
-export default ActionTypes;
